b50832de8afe9bba17e954ab887d682cd32e272f
Love  Louidor <105249391+Louidor-Love@users.noreply.github.com>
2024-05-23 20:53:58 -0300

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,12 @@
+list1 =[{"nombre" : "julio",
+        "edad" : 20,
+        "estatura" : 1.80},
+        {"nombre" : "julia",
+        "edad" : 24,
+        "estatura" : 1.50},
+        {"nombre" : "julio",
+        "edad" : 20,
+        "estatura" : 1.80},]
+
+for i in list1:
+    print(i)
